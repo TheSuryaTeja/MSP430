2b894ae4485531b37b94607c68d82803c78a974a
Surya Teja <64546112+TheSuryaTeja@users.noreply.github.com>
2020-11-03 21:09:30 +0530

    Update code.c

--- SWITCH/code.c
+++ SWITCH/code.c
@@ -3,9 +3,9 @@
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;   // stop Watch dog timer
+    WDTCTL = WDTPW | WDTHOLD;   	// stop Watch dog timer
 
-    P1DIR |= BIT7; 				// P1.7 set as output
+    P1DIR |= BIT7; 			// P1.7 set as output
     P1DIR &= ~BIT6; 			// P1.6 set as input
 
 while(1)
@@ -13,9 +13,9 @@ while(1)
 	{
 
     	if(P1IN & BIT6) 
-        	while(P1IN & BIT6) 		// while the switch is pressed
-            	 P1OUT |= BIT7; 	// P1.7 is high
+        	while(P1IN & BIT6) 	// while the switch is pressed
+            	 P1OUT |= BIT7; 	// set P1.7 as high
     	else
-        	P1OUT &= ~BIT7;    		// P1.7 is low
+        	P1OUT &= ~BIT7;    	// set P1.7 as low
 	}
 }
